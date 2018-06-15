4658a706cfb21574c5cb285a70446e600a6f106a
DESKTOP-AJ93D3H\Student <s170015@edu.tsuruoka-nct.ac.jp>
2018-06-15 19:00:38 +0900

    aaaaaaaaa

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,1 +1,33 @@
+import numpy as np
+import math
+HALF_STRING = 8
+MAX_STRING = 2*HALF_STRING
+MAX_STEP = 50
+MAX_POP = 500
+class Individual:
+    def __init__(self, chrom[MAX_STRING], fitness):
+        self.chrom[MAX_STRING] = chrom[MAX_STRING]
+        self.fitness = fitness
+    def getChrom(self):
+        return self.chrom[MAX_STRING]
+    def getFitness(self):
+        return self.fitness
 
+chromSize
+nPop, nGen
+Pm, Pc
+funcSeed = 1
+Table2ConvBinCode2DecimalNumber = [1,2,4,8,16,32,64,128,256,512]
+biasY, normF
+kindOfHill = 0
+gen, minStep
+sumFitness
+bestChrom[MAX_STRING]
+maxFitness, minFitness, avgFitness, bestFitness
+maxIndiv
+normD
+
+MAX_BREAK = 20
+M_PEAKS = 30
+
+def main():
